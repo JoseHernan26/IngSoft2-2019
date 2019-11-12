0a268d2bfb9d08bb82a0ca040911436da9c058af
nucky1 <51407707+nucky1@users.noreply.github.com>
2019-11-12 11:14:15 -0300

    Add files via upload
    Lo del jaita

---
+++ Interfaces/VentaMVC.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Interfaces;
+
+import Modelos.DetalleVenta;
+import Modelos.Producto;
+import Modelos.Venta;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author aleja
+ */
+public class VentaMVC {
+    public interface DAO{
+        public ArrayList<Producto> obtenerProductos();
+        public int insertVenta(Venta venta);
+    }
+    
+    public interface Controler{
+        public void insertarVenta(Venta venta);
+        public ArrayList<Producto> obtenerTodo();
+    }
+    
+    public interface View{
+        public void buscarProducto();
+        public void mostrarExito();
+        public void mostrarError(String error);
+    }
+}
---
+++ Modelos/Equipo.java
@@ -0,0 +1,106 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modelos;
+import java.util.Date;
+
+/**
+ *
+ * @author Note250
+ */
+
+public class Equipo {
+    private int id;
+    private int idCliente;
+    private String detallesIngreso;
+    private String motivoFalla;
+    private Date fechaRecepcion;
+    private estadoEquipo estado;
+    private String modelo;
+    private String marca;
+
+    public Equipo(int id, int idCliente, String detallesIngreso, String motivoFalla, Date fechaRecepcion, estadoEquipo estado, String modelo, String marca) {
+        this.id = id;
+        this.idCliente = idCliente;
+        this.detallesIngreso = detallesIngreso;
+        this.motivoFalla = motivoFalla;
+        this.fechaRecepcion = fechaRecepcion;
+        this.estado = estado;
+        this.modelo = modelo;
+        this.marca = marca;
+    }
+
+    public Equipo() {
+        
+    }
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getIdCliente() {
+        return idCliente;
+    }
+
+    public void setIdCliente(int idCliente) {
+        this.idCliente = idCliente;
+    }
+
+    public String getDetallesIngreso() {
+        return detallesIngreso;
+    }
+
+    public void setDetallesIngreso(String detallesIngreso) {
+        this.detallesIngreso = detallesIngreso;
+    }
+
+    public String getMotivoFalla() {
+        return motivoFalla;
+    }
+
+    public void setMotivoFalla(String motivoFalla) {
+        this.motivoFalla = motivoFalla;
+    }
+
+    public Date getFechaRecepcion() {
+        return fechaRecepcion;
+    }
+
+    public void setFechaRecepcion(Date fechaRecepcion) {
+        this.fechaRecepcion = fechaRecepcion;
+    }
+
+    public estadoEquipo getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estadoEquipo.valueOf(estado);
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    
+
+    
+    
+}
